79c07bf45d36bd22f68919fe7e0a1c696c072e83
ckw28502 <ckw28502@gmail.com>
2024-05-21 19:11:55 +0200

    feat: create guard for unauthorized users

---
+++ src/app/shared/guards/is-logged-in.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const isLoggedInGuard: CanActivateFn = () => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true;
+  } else {
+    router.navigate(["/login"]);
+    return false;
+  }
+};
