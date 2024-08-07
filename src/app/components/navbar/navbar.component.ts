5c9f99201e3c3dec4966bd86cb64d01b88ea022d
ckw28502 <ckw28502@gmail.com>
2024-08-07 04:27:59 +0200

    feat: implement navbar menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,26 +3,80 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import  {MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgIf } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Role } from '../../shared/models/enums/role';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, NgIf],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, RouterLink, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
+  menus = {
+    [Role.SALES]: [
+      {
+        title: "DASHBOARD",
+        url: "/"
+      },
+      {
+        title: "CLIENTS",
+        url: "/clients"
+      },
+      {
+        title: "PRODUCTS",
+        url: "/products"
+      },
+      {
+        title: "TRANSACTIONS",
+        url: "/transactions"
+      }
+    ],
+    [Role.PROCUREMENT]: [
+      {
+        title: "DASHBOARD",
+        url: "/"
+      },
+      {
+        title: "SUPPLIER",
+        url: "/suppliers"
+      },
+      {
+        title: "PRODUCTS",
+        url: "/products"
+      },
+      {
+        title: "transactions",
+        url: "/transactions"
+      }
+    ],
+    [Role.HR]: [
+      {
+        title: "DASHBOARD",
+        url: "/"
+      },
+      {
+        title: "EMPLOYEES",
+        url: "/employees"
+      }
+    ]
+  }
+
+  selectedMenu: Array<{title: string, url: string}> = [];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     const token: string | null = sessionStorage.getItem("token");    
-
     this.isLoggedIn = token !== null;
+    
+    const role: Role | null = sessionStorage.getItem("role") as Role;
+    if (role) {
+      this.selectedMenu = this.menus[role];
+    }
   }
 
   logout(): void {
